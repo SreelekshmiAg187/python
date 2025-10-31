0caa069458cc69b65134ff5c5bc5c665e8f9b381
SreelekshmiAg187 <sreelekshmiag187@gmail.com>
2025-10-31 12:45:20 +0530

    Add files via upload

---
+++ co3/main1.py
@@ -0,0 +1,11 @@
+from math1  import arithametic, rectangle
+from math1. advance import triangle
+
+print("Rectangle area:", rectangle.area(10,5))
+print("Rectangle perimeter:", rectangle.perimeter(10,5))
+
+print("Addition:", arithametic.add(20, 5))
+print("substraction:", arithametic.sub(20, 5))
+
+print("triangle_area:", triangle.tri_area(5,4))
+      
---
+++ co3/math1/rectangle.py
@@ -0,0 +1,4 @@
+def area(length, breadth):
+       return length * breadth
+def  peimeter(length, breadth):
+       return 2 * (length + breadth)
