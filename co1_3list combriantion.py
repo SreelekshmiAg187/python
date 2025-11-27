809a725dfe31e69a5d085ebe13018e5cf26450ef
SreelekshmiAg187 <sreelekshmiag187@gmail.com>
2025-11-27 14:18:05 +0530

    Add files via upload

---
+++ co1_3list combriantion.py
@@ -0,0 +1,8 @@
+#generate positive list of numbers from a given list of numbers
+#pn=[i for i in n if i > 0]
+#print(pn)
+
+#square of N numbers
+s=int(input("enter the numbers:"))
+v=[ j*j for j in range(s) ]
+print(v)
